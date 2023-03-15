3a4608ea9e4a7aa17f7516a61cf78f0b24cdf897
Warren R Bank <warren.r.bank@gmail.com>
2023-03-15 14:44:05 -0700

    add sample audio streams

--- lambda/constants.js
+++ lambda/constants.js
@@ -1,20 +1,44 @@
-exports.audioData = [
-];
+// https://mp3.pm/
 
-/** Example format
 exports.audioData = [
   {
-    title: 'Title',
-    url: 'https://domain.com/test.mp3',
+    title: "I'm Gonna Be Strong by Blue Angel from 1980 with Cyndi Lauper",
+    url: 'https://cs1.mp3.pm/listen/143271014/TnNmUG9HME1QcHYxWlY1NWxDcFdDV1Z4eEdFRTg2amZwK09YN29DdFpiMkZHeFNHUHpHblNJdm5iUHJmTUx1M2hqUzh1elB4eEpFQ25nV0RlYnpOVDVyV1NNS0I3ekdJbHlyd2E2Z3FWYkt6a01abHVtdndObElEQ1c3REtSRlM/Blue_Angel_-_I_m_Gonna_Be_Strong_(mp3.pm).mp3'
   },
   {
-    title: 'Title 2',
-    url: 'https://domain.com/test2.mp3',
+    title: "Saint Cecilia by Blue Oyster Cult from 1970",
+    url: 'https://cs1.mp3.pm/listen/53292465/TnNmUG9HME1QcHYxWlY1NWxDcFdDV1Z4eEdFRTg2amZwK09YN29DdFpiMWJBTzdCcWxvdzlycDg5b2R4K3VXb3dTa2N0Wk40RkxTYzZPd0VtbGMxWEVEUUdBTzRwWlFCVUlXb3hTU2FKalNNeG9nNVYwaWYrSXUrWmZhNUlGdUE/Stalk-Forrest_Group_-_St._Cecilia_original_version_(mp3.pm).mp3'
   },
   {
-    title: 'Title 3',
-    url: 'https://domain.com/test3.mp3',
+    title: "Astronomy by Blue Oyster Cult from 1974 with piano",
+    url: 'https://cs1.mp3.pm/listen/34812133/TnNmUG9HME1QcHYxWlY1NWxDcFdDV1Z4eEdFRTg2amZwK09YN29DdFpiMWJaN2lGTVlmMHNOZVB3TG9pYWYvdnp1NXA1UEhIRzZ5b21QQU8xSS9zK3JGb3hwU0xQT2cwK0JibUlINWcrOGtIRVl4ekwxbzAwL0JHNFdYdEorS28/Blue_Oyster_Cult_-_Astronomy_1974_-_pesnya_naibolee_izvestna_kaver-versiej_gruppy_Metallica_98_(mp3.pm).mp3'
+  },
+  {
+    title: "Astronomy by Blue Oyster Cult from 1978 with guitar",
+    url: 'https://cs1.mp3.pm/listen/143088817/TnNmUG9HME1QcHYxWlY1NWxDcFdDV1Z4eEdFRTg2amZwK09YN29DdFpiMnNScDdOK2NPS2pxY3pGWC82L05nc0VDMlQ3MDgrR1JJTzk3KzVEenQ1bklLVFpWWHlXSmplUGV0WkZzV1ROZVdWYmRkd2g0VTg1VUZ4OFZMVEw3Ykw/Blue_Oyster_Cult_-_Astronomy_Some_Enchanted_Evening_(mp3.pm).mp3'
+  },
+  {
+    title: "Shooting Shark by Blue Oyster Cult with lyrics by Patti Smith",
+    url: 'https://cs1.mp3.pm/listen/57785169/TnNmUG9HME1QcHYxWlY1NWxDcFdDV1Z4eEdFRTg2amZwK09YN29DdFpiMHRLOGdndVZjRW1od3FhQUQvSjE1aXZQMmhxR3F4WE1ta2szQ2R4S3FYZDRjRU10bUhleVdvU3h2cmY5YUZHU0VKaWhvMFVIdk5HNFNmTWloTkx6b0g/Blue_Oyster_Cult_-_Shooting_Shark_(mp3.pm).mp3'
+  },
+  {
+    title: "Kimberly from 1976 by Patti Smith",
+    url: 'https://cs1.mp3.pm/listen/92047682/TnNmUG9HME1QcHYxWlY1NWxDcFdDV1Z4eEdFRTg2amZwK09YN29DdFpiMkpZblZRcFlmYVdub29Ecm5HSlY1VEJ3aUdxVFA0eGhTRjU5NnorUDRPcUdZUVNuMXRYZjdHS2FoZFZObXRidFdmTkphbnZxbWk4UmpaSFJBenNwMmc/Patti_Smith_-_Kimberly_Live_at_the_Boarding_House_San_Francisco_1976_(mp3.pm).mp3'
+  },
+  {
+    title: "Ain't It Strange from 1976 by Patti Smith",
+    url: 'https://cs1.mp3.pm/listen/4558255/TnNmUG9HME1QcHYxWlY1NWxDcFdDV1Z4eEdFRTg2amZwK09YN29DdFpiM1cyejJsaEoxdFZZeWdGTlpMUEt3MzZCOHROY3BoZUJQc3VZbUNLUWFEZFFyOFE0ZjlPSVkwSGZuWHNOTDRtemVMQXMxVVRoZy8vVmFjNXJCSU05R1o/PATTI_SMITH_-_Radio_Ethiopia_1976_-_02._Ain_t_It_Strange_(mp3.pm).mp3'
+  },
+  {
+    title: "Ghost Dance from 1978 by Patti Smith",
+    url: 'https://cs1.mp3.pm/listen/167605236/TnNmUG9HME1QcHYxWlY1NWxDcFdDV1Z4eEdFRTg2amZwK09YN29DdFpiMW9rYllPRE9reE9Zdnk5MVYvOWpvY2tQYnJMbkxDMzl6LytBRU9pS2VkNFQzblFHb0dscFdVbmhDMWsxV2lRUGVZT2VUQzdyQjNOYlZBdUFrdzYwV3g/Patti_Smith_Group_-_Ghost_Dance_(mp3.pm).mp3'
+  },
+  {
+    title: "Privilege from 1978 by Patti Smith",
+    url: 'https://cs1.mp3.pm/listen/7211197/TnNmUG9HME1QcHYxWlY1NWxDcFdDV1Z4eEdFRTg2amZwK09YN29DdFpiMnpjTXd5NVpQRzlCZnNLSUp1SnBqMDFSeVVJUVdscUorMWhqRVA2U0t0THNvZHlpSzN2TlpaMC9INUJwczVSNWtwdUFHOUhJMTQ3Q2VOUFZCWHZOR2U/Patti_Smith_-_Privilege_set_Me_Free_(mp3.pm).mp3'
+  },
+  {
+    title: "25th Floor from 1978 by Patti Smith",
+    url: 'https://cs1.mp3.pm/listen/116951352/TnNmUG9HME1QcHYxWlY1NWxDcFdDV1Z4eEdFRTg2amZwK09YN29DdFpiMmZ4dlVqZC92WjJQOVduRW1kdWx5NlhzbGhLVE8yZmhTOU12REpoTlJKc2Nvc2FVNDhMcHp2Z0tXUDFWOW0rY3RiTjlMUWEweE9KV0RGUlA5cjRmUS8/PATTI_SMITH_-_25th_Floor_(mp3.pm).mp3'
   }
 ];
-
-**/
